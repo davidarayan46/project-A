56cabff50558f3964ea35951f70660b4f7e12a3d
davidaryan46 <ajesajith@gmail.com>
2023-11-02 16:33:52 +0530

    new update

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,26 @@
+package org.company; //(basic program)
+
+public class CompanyInfo {
+
+	private void empName() {
+		// TODO Auto-generated method stub
+System.out.println("emplyee name is ajith");
+	}
+private void empid() {
+	// TODO Auto-generated method stub
+System.out.println("emplyee id is 1762826");
+}
+private void empNative() {
+	// TODO Auto-generated method stub
+System.out.println(" empylee native is ariyalur");
+}
+
+public static void main(String[] args) {
+	CompanyInfo a=new CompanyInfo();
+	a.empid();
+	a.empName();
+	a.empNative();
+	}
+
+
+}
